55deeaabc0b2776f446ba14aa9ce4ce353441a9b
lito00356 <tomasz.liksza@gmail.com>
2025-05-14 23:28:04 +0200

    Added progress titles and cards

---
+++ myapp/main.js
@@ -0,0 +1,13 @@
+const $openMenu = document.getElementById("open-menu");
+const $closeMenu = document.getElementById("close-menu");
+const $filter = document.getElementById("filter");
+
+$openMenu.addEventListener("click", function (event) {
+  console.log("hallo");
+});
+$closeMenu.addEventListener("click", function (event) {
+  console.log("hallo");
+});
+$filter.addEventListener("click", function (event) {
+  console.log("hallo");
+});
