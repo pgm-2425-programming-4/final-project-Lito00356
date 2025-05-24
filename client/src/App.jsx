d2f0718a010f21a8f289c92ab848bcdab93ae4fc
lito00356 <tomasz.liksza@gmail.com>
2025-05-25 00:05:01 +0200

    added jsx to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,118 @@ const queryClient = new QueryClient();
 export default function MyApp() {
   return (
     <QueryClientProvider client={queryClient}>
-      <PaginatedBacklog />
+      {/* <PaginatedBacklog /> */}
+      <h1 className="title">Project Title</h1>
+      <section className="tasks-container">
+        <div className="tasks" id="to-do">
+          <strong className="tasks__title">To Do</strong>
+          <article className="tasks__item">
+            <form className="form">
+              <label className="form__label">
+                <input className="form__input" type="text" placeholder="Title" />
+              </label>
+
+              <textarea className="form__textarea" type="text" placeholder="Description"></textarea>
+            </form>
+          </article>
+        </div>
+
+        <div className="tasks" id="in-progress">
+          <strong className="tasks__title">In progress</strong>
+          <article className="tasks__item">
+            <form className="form">
+              <label className="form__label">
+                <input className="form__input" type="text" placeholder="Title" />
+              </label>
+
+              <textarea className="form__textarea" type="text" placeholder="Description"></textarea>
+            </form>
+          </article>
+        </div>
+
+        <div className="tasks" id="in-progress">
+          <strong className="tasks__title">Ready for review</strong>
+          <article className="tasks__item">
+            <form className="form">
+              <label className="form__label">
+                <input className="form__input" type="text" placeholder="Title" />
+              </label>
+
+              <textarea className="form__textarea" type="text" placeholder="Description"></textarea>
+            </form>
+          </article>
+        </div>
+
+        <div className="tasks" id="in-progress">
+          <strong className="tasks__title">Done</strong>
+          <article className="tasks__item">
+            <form className="form">
+              <label className="form__label">
+                <input className="form__input" type="text" placeholder="Title" />
+              </label>
+
+              <textarea className="form__textarea" type="text" placeholder="Description"></textarea>
+            </form>
+          </article>
+        </div>
+      </section>
+
+      <div className="menu-items">
+        <div className="scroll-menu" id="menu">
+          <div className="menu__current-project">
+            <strong>Project title</strong>
+            <a href="#">Backlog</a>
+          </div>
+          <div className="menu__projects">
+            <strong className="menu__projects-title">Other projects</strong>
+            <a href="">Project 1</a>
+            <a href="">Project 2</a>
+            <a href="">Project 3</a>
+            <a href="">Project 4</a>
+            <a href="">Project 5</a>
+            <a href="">Project 6</a>
+            <a href="">Project 7</a>
+            <a href="">Project 8</a>
+            <a href="">Project 9</a>
+            <a href="">Project 10</a>
+            <a href="">Project 11</a>
+            <a href="">Project 12</a>
+            <a href="">Project 13</a>
+            <a href="">Project 14</a>
+          </div>
+        </div>
+        <button className="button hamburger" id="closed-menu">
+          <svg className="hamburger-opened" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+              <path d="M4 18H10" stroke="#000000" strokeWidth="2" strokeLinecap="round" />
+              <path d="M4 12L16 12" stroke="#000000" strokeWidth="2" strokeLinecap="round" />
+              <path d="M4 6L20 6" stroke="#000000" strokeWidth="2" strokeLinecap="round" />
+            </g>
+          </svg>
+        </button>
+        <button className="button" id="opened-menu">
+          <svg className="hamburger-opened" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+              <path d="M4 18H10" stroke="#000000" strokeWidth="2" strokeLinecap="round" />
+              <path d="M4 12L16 12" stroke="#000000" strokeWidth="2" strokeLinecap="round" />
+              <path d="M4 6L20 6" stroke="#000000" strokeWidth="2" strokeLinecap="round" />
+            </g>
+          </svg>
+        </button>
+        <button className="button search" id="filter">
+          <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+              <path d="M14.9536 14.9458L21 21M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            </g>
+          </svg>
+        </button>
+      </div>
     </QueryClientProvider>
   );
 }
