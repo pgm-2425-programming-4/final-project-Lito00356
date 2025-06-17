75adad580e6c89ce294a73420ed11f6a2fcd0ea1
lito00356 <tomasz.liksza@gmail.com>
2025-06-17 14:18:00 +0200

    building fetch functions

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,4 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import MyApp from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <MyApp />
-  </StrictMode>,
-);
+createRoot(document.getElementById("root")).render(<MyApp />);
