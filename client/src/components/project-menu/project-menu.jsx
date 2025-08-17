c0acd30cbbeaf37f67c174e76575778d6789b3f5
lito00356 <tomasz.liksza@gmail.com>
2025-08-17 22:03:19 +0200

    Some styling fixes on menu in mobile view

--- client/src/components/project-menu/project-menu.jsx
+++ client/src/components/project-menu/project-menu.jsx
@@ -3,8 +3,6 @@ import { Link } from "@tanstack/react-router";
 import { PaginatedBacklog } from "../backlog/paginated-backlog";
 
 export function ProjectMenu({ projects = [] }) {
-  console.log(projects);
-
   const [openMenu, setOpenMenu] = useState(false);
   const [openProjects, setOpenProjects] = useState(false);
 
@@ -22,7 +20,7 @@ export function ProjectMenu({ projects = [] }) {
 
   return (
     <>
-      <button className={`button hamburger ${openMenu ? "open" : ""}`} id="closed-menu" onClick={openMenu ? closeProjectMenu : openProjectMenu}>
+      <button className={`button hamburger ${openMenu ? "open" : ""}`} id="closed-menu" type="button" onClick={openMenu ? closeProjectMenu : openProjectMenu}>
         <svg className="" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
           <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
