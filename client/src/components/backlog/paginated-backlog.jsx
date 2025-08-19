c58fb7d31bf9edd1c5d45576258a59c8c2219dcd
lito00356 <tomasz.liksza@gmail.com>
2025-08-19 23:14:15 +0200

    Rewritten a lot of backlog code

--- client/src/components/backlog/paginated-backlog.jsx
+++ client/src/components/backlog/paginated-backlog.jsx
@@ -1,18 +1,29 @@
-import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
-import { getPaginatedBacklogTasks } from "../../queries/get-backlog-tasks";
+import { useState, useEffect } from "react";
 import { BacklogList } from "./backlog-list/backlog-list";
 import { Pagination } from "./pagination/pagination";
 
-export function PaginatedBacklog() {
+export function PaginatedBacklog({ selectedProject, isPending, isError, error }) {
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(10);
+  const [tasks, setTasks] = useState([]);
 
-  const { isPending, isError, data, error } = useQuery({
-    queryKey: ["tasks", currentPage, pageSize],
-    queryFn: () => getPaginatedBacklogTasks(currentPage, pageSize),
-    staleTime: 1000,
-  });
+  // const { isPending, isError, data, error } = useQuery({
+  //   queryKey: ["tasks", currentPage, pageSize],
+  //   queryFn: () => getPaginatedBacklogTasks(currentPage, pageSize),
+  //   staleTime: 1000,
+  // });
+
+  useEffect(() => {
+    console.log(selectedProject);
+
+    setCurrentPage(1);
+  }, [selectedProject]);
+
+  useEffect(() => {
+    if (selectedProject) {
+      fetchBacklogItems(selectedProject, pageSize, currentPage);
+    }
+  }, [selectedProject, currentPage, pageSize]);
 
   function handlePageChanged(pageNumber) {
     setCurrentPage(pageNumber);
@@ -23,16 +34,34 @@ export function PaginatedBacklog() {
     setCurrentPage(1);
   }
 
+  async function fetchBacklogItems(project, pageSize, currentPage) {
+    let start = pageSize * (currentPage - 1);
+    let tasks = [];
+    for (let i = start; i < Math.min(start + pageSize, project.tasks.length); i++) {
+      tasks.push(project.tasks[i]);
+    }
+    setTasks(tasks);
+  }
+
   if (isPending) return <span>Loading...</span>;
   if (isError) return <span>Error: {error.message}</span>;
 
-  const tasks = data.data;
-  const pageCount = data.meta.pagination.pageCount;
-
   return (
     <>
-      <BacklogList tasks={tasks} />
-      <Pagination currentPage={currentPage} pageCount={pageCount} pageSize={pageSize} onPageChanged={handlePageChanged} onPageSizeChanged={handlePageSizeChanged} />
+      {selectedProject ? (
+        <>
+          <div className="outlet__title">
+            <small>for</small>
+            <h2>{selectedProject.projectName}</h2>
+          </div>
+          <BacklogList tasks={tasks} />
+          <Pagination currentPage={currentPage} pageCount={Math.ceil(selectedProject.tasks.length / pageSize)} pageSize={pageSize} onPageChanged={handlePageChanged} onPageSizeChanged={handlePageSizeChanged} />
+        </>
+      ) : (
+        <div>
+          <small>Select a project</small>
+        </div>
+      )}
     </>
   );
 }
--- client/src/routes/backlog.jsx
+++ client/src/routes/backlog.jsx
@@ -1,28 +1,62 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { PaginatedBacklog } from "../components/backlog/paginated-backlog";
+import { useQuery } from "@tanstack/react-query";
+import { Link } from "@tanstack/react-router";
+import { getProjects } from "../queries/get-projects";
+import { useState, useEffect } from "react";
 
 export const Route = createFileRoute("/backlog")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const [selectedProject, setSelectedProject] = useState(null);
+  const [params, setParams] = useState(Route.useParams());
+
+  const {
+    data: projects,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["projects"],
+    queryFn: () => getProjects(),
+  });
+
+  function handleClick(documentId) {
+    setParams((prevParams) => ({
+      ...prevParams,
+      projectId: documentId,
+    }));
+  }
+
+  useEffect(() => {
+    if (projects && params.projectId) {
+      const foundProject = projects.find((project) => project.documentId === params.projectId);
+      setSelectedProject(foundProject);
+    }
+  }, [params.projectId, projects, params]);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading project.</div>;
+  if (!projects) return <div>Project not found.</div>;
+
   return (
     <>
       <main className="backlog-container">
         <div className="projects-container">
           <h1>Backlog</h1>
           <ul className="projects-list">
-            <li className="projects-list-item">project1</li>
-            <li>project2</li>
-            <li>project3</li>
+            {projects.map((project) => (
+              <li key={project.id} className="projects-list-item">
+                <Link to={`/backlog?projectId=${project.documentId}`} onClick={() => handleClick(project.documentId)}>
+                  {project.projectName}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
         <div className="outlet">
-          <div className="outlet__title">
-            <small>for</small>
-            <h2> Project title</h2>
-          </div>
-          <PaginatedBacklog />
+          <PaginatedBacklog selectedProject={selectedProject} isLoading={isLoading} error={error} />
         </div>
       </main>
     </>
